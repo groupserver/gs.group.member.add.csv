8ca4d7ea39f83f92be6acd77ff754e97a387a385
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-23 13:30:10 -0800

    Upping the Copyright date
    --HG--
    extra : rebase_source : afb7425078e134984013dd7c7fb2b9715ced9974

--- setup.py
+++ setup.py
@@ -1,36 +1,66 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-setup(name='gs.group.member.invite',
+setup(name='gs.group.member.add.csv',
     version=version,
-    description="The group-related pages that are required for "\
-        "administrator-initiated sign up (registration)",
+    description="Add people to a group using a CSV file",
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    keywords='sign up, registration, profile, user, join',
-    author='Michael JasonSmith',
-    author_email='mpj17@onlinegroups.net',
+    keywords='sign up, registration, profile, user, join, invitation',
+    author='Richard Waid',
+    author_email='richard@onlinegroups.net',
     url='http://groupserver.org',
-    license='ZPL',
+    license='ZPL 2,1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member'],
+    namespace_packages=['gs', 'gs.group', 'gs.group.member',
+                            'gs.group.member.add'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'zope.browserpage',
+        'zope.cachedescriptors',
+        'zope.formlib',
+        'zope.viewlet',
+        'gs.content.form',
+        'gs.group.member.add.json',
+        'gs.help',
+        'gs.profile.email.base',
+        'gs.profile.notify',
+        'gs.site.member',
         'Products.GSProfile',
-        # -*- Extra requirements: -*-
+        'Products.XWFCore',
+        'Products.CustomUserFolder'
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,
 )
-
