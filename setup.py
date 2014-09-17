a7b99b909f76b812658ff20f7d87e6b4b9fe11a8
alicemurphy <ajr@posteo.de>
2014-09-17 13:49:33 -0700

    Naming .rst files as such
    --HG--
    rename : README.txt => README.rst
    rename : docs/HISTORY.txt => docs/HISTORY.rst
    extra : rebase_source : 0b7d67eeb8c4269db2bdaa9ae106f34c3405c36b

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ version = get_version()
 setup(name='gs.group.member.add.csv',
     version=version,
     description="Add people to a group using a CSV file",
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=open("README.rst").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.rst")).read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
